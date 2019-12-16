57d9abdce1354734eb269d565e9fd3724cda1b2a
Scott Appleton <scottappleton09@gmail.com>
2019-12-16 13:43:48 -0800

    .

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const port = 5000 || process.env.PORT;
+const app = express();
+const cors = require('cors');
+const request = require('request');
+require('dotenv').config();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+});
+
+app.get('/jobs', (req, res) => {
+  console.log("here")
+  const url = `https://api.ziprecruiter.com/jobs/v1?search=${req.query.jobSearch}&location=${req.query.location}&radius_miles=${req.query.radius_miles}&days_ago=${req.query.days_ago}&api_key=${process.env.ZIP_API}`;
+  console.log(url);
+  request(
+    url,
+    (error, result) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(result.body);
+      res.status(200).send(result.body);
+    }
+  )
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on ${port}`)
+});
