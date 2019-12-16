c5032ed86c1f081aea8a220582143cadf085310c
Scott Appleton <scottappleton09@gmail.com>
2019-12-15 22:15:54 -0800

    Bring in ControlledOpenSelect Component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Footer from '../src/components/Footer/Footer.component';
 import { TextField } from '@material-ui/core';
 import { Button } from '@material-ui/core';
-import { Container } from '@material-ui/core';
 import JobCard from '../src/components/JobCard/Jobcard.component';
-import SimpleMenu from '../src/components/SimpleMenu/SimpleMenu.component';
+import ControlledOpenSelect from '../src/components/ControlledOpenSelect/ControlledOpenSelect';
+
+
 
 function App() {
 
@@ -40,38 +41,24 @@ function App() {
 
   return (
     <div className="App">
-
       <div>
-      
         <form className="inputform" onSubmit={handleSubmit} >
-
-            
             <TextField id="standard-basic" label="Job description or keyword" value={jobSearch} onChange={e => setJobSearch(e.target.value)} type="text" name="jobdescription"/>
             <TextField id="standard-basic" label="Near what city?" value={city} onChange={e => setCity(e.target.value)} type="text" name="city"/>
-          
-
             <Button className="button" type="submit" variant="contained" color="primary">
             Find your new job!
           </Button>
         </form>
         <div className="results">
           { jobs ? <p>{jobs.length} results found</p> : null }
+          <ControlledOpenSelect />
         </div>
-
-      
-        
       </div>
 
-      
-      
-      
       <main className="job-content">
-        
-
         { jobs ? jobs.map((job) => {
           return <JobCard url={job.url} name={job.name} age={job.job_age} company={job.hiring_company.name} />
         }) : null}
-      
       </main>
         <Footer />
     </div>
