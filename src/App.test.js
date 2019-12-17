ef7cf82be990ef987bfb1c513fe0be9429eed8ec
Scott Appleton <scottappleton09@gmail.com>
2019-12-16 21:59:39 -0800

    Add test to check input field has correct label

--- src/App.test.js
+++ src/App.test.js
@@ -10,10 +10,19 @@ test('renders title properly in app', () => {
 });
 
 test('check for input with label Near what city?', () => {
-  const {getByLabelText} = render(<App />);
+  const { getByLabelText } = render(<App />);
   const input = getByLabelText(/Near what city?/);
   expect(input).toBeInTheDocument();
-})
+});
+
+test('check for input field with label Job description or keyword', () => {
+  const { getByLabelText } = render(<App />);
+  const input = getByLabelText(/Job description or keyword/);
+  expect(input).toBeInTheDocument();
+});
+
+
+
 
 
 
