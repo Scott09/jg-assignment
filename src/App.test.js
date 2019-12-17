00129313b8063a73e535b7c6387c333388a938d8
Scott Appleton <scottappleton09@gmail.com>
2019-12-16 21:50:43 -0800

    Add test to make sure header is rendered in App component

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders title properly in app', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = getByText(/Let us help you on your job search!/);
+  expect(title).toBeInTheDocument();
 });
+
+test('check for input with label Near what city?', () => {
+  const {getByLabelText} = render(<App />);
+  const input = getByLabelText(/Near what city?/);
+  expect(input).toBeInTheDocument();
+})
+
+
+
